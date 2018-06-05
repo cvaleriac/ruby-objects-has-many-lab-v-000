0a3739798b478d8ea60cc5ba2eebd6c4e50c8fb4
valeria campilongo <cvaleriac@hotmail.it>
2018-06-05 19:06:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name
 
-  song_count = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -22,11 +22,10 @@ class Artist
   end
 
   def songs
-    @songs
+    @songs 
   end
 
   def self.song_count
     @@song_count
   end
-
 end
