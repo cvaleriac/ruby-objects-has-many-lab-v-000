86ed8f7796dae69495a01e8986e9fc422d21c5e7
valeria campilongo <cvaleriac@hotmail.it>
2018-06-05 19:01:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,17 +8,17 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)	
-    self.songs << song	
-    song.artist = self	
-    @@song_count +=1	
-  end	
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
 
-  def add_song_by_name(name)	
-    song = Song.new(name)	
-    self.songs << song	
-    song.artist = self	
-    @@song_count +=1	
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
   end
 
   def songs
